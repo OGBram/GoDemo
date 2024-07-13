fc1cb391f7648b441e66f2dccb8cc51f5fab4118
OGBram <156800524+OGBram@users.noreply.github.com>
2024-07-13 15:06:51 -0700

    .0

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 var version float64 = 1.01
 var author string = "Bryan Blackburn"
+var PORT int = 8080
 
 func main() {
+
+	http.ListenAndServe(":8080", nil)
+
+	fmt.Printf("Server Started on Port: %v \n", PORT)
 	fmt.Printf("%v, Version:%v", author, version)
+
 }
