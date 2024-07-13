580f58709db9cd22adcf737f90b5db808606f8e3
OGBram <156800524+OGBram@users.noreply.github.com>
2024-07-13 15:26:47 -0700

    a

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GoDemo
- 
+ Bryan Blackburn
+ Artist / IT
+ 520-286-3225
--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-var version float64 = 1.01
-var author string = "Bryan Blackburn"
-var PORT int = 8080
+// handler function http server
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	//switch example with cases for Methods
+
+	switch r.Method {
+
+	case http.MethodGet:
+		fmt.Fprintf(w, "Connected to Server")
+
+	case http.MethodPost:
+		fmt.Fprintf(w, "Hello, POST method!")
+
+	case http.MethodPut:
+		fmt.Fprintf(w, "Hello, PUT method!")
+
+	case http.MethodDelete:
+		fmt.Fprintf(w, "Hello, DELETE method!")
+
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
 
 func main() {
+	// Register the handler function for the root URL path
+	http.HandleFunc("/", helloHandler)
 
-	http.ListenAndServe(":8080", nil)
+	PORT := ":8080"
 
-	fmt.Printf("Server Started on Port: %v \n", PORT)
-	fmt.Printf("%v, Version:%v", author, version)
-
+	log.Printf("Starting server on %s\n", PORT)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v\n", err)
+	}
 }
